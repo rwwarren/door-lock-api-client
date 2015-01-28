0f35ce4b4cf89114ea3f86b45626263fa58d3b57
Ryan Warren <ryan@war.re>
2015-01-28 00:46:09 -0800

    starting with api work

--- src/root/apiClient.php
+++ src/root/apiClient.php
@@ -1,6 +1,18 @@
 <?php
 
+
+
 namespace ApiClient;
+
+//require __DIR__.'/../../../door-lock/web/src/vendor/predis/predis/src/Autoloader.php';
+////require __DIR__.'/../../../../door-lock/web/src/vendor/predis/predis/src/Autoloader.php';
+//\Predis\Autoloader::register();
+//$client = new \Predis\Client([
+//  'scheme' => 'tcp',
+//  'host'   => '127.0.0.1',
+//  //'host'   => '10.0.0.1',
+//  'port'   => 6379,
+//]);
 
 class ApiClient{
 
@@ -20,14 +32,14 @@ class ApiClient{
     header("Access-Control-Allow-Methods: *");
   }
 
-  public function login($params){
+  public function login($params, $cookie){
 //    $this->setUpRequest();
     //open connection
     $ch = curl_init();
 
     //set the url, number of POST vars, POST data
     curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/login");
-    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey"));
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
 //    curl_setopt($ch,CURLOPT_POST, count($fields));
 //    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
     curl_setopt($ch,CURLOPT_POSTFIELDS, array('username'=>$params['Username'],'password'=>$params['Password']));
@@ -43,6 +55,34 @@ class ApiClient{
 
   public function logout(){
 
+  }
+
+  public function isLoggedIn($sid){
+//    echo __DIR__;
+//    global $client;
+//    $userID = $client->hget('apiKeys', sid);
+    include_once __DIR__.'/../../../door-lock/web/src/vendor/predis/predis/src/Autoloader.php';
+//    require_once __DIR__.'/../../../door-lock/web/src/vendor/predis/predis/src/Autoloader.php';
+//require __DIR__.'/../../../../door-lock/web/src/vendor/predis/predis/src/Autoloader.php';
+    \Predis\Autoloader::register();
+    $client = new \Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => '127.0.0.1',
+      //'host'   => '10.0.0.1',
+      'port'   => 6379,
+    ]);
+//    echo $client->hget('loggedInUsers', $sid);
+    return $client->hget('loggedInUsers', $sid);
+//    return
+//    return isset($_SESSION['username']) && $_SESSION['username'] !== null;
+  }
+
+  public function isAdmin($sid){
+//    echo __DIR__;
+    global $client;
+    $userID = $client->hget('apiKeys', sid);
+//    return
+    return isset($_SESSION['username']) && $_SESSION['username'] !== null;
   }
 
   public function changePassword(){
