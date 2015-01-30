ad3474572c016ed158bcaf18163e56e432af6ffc
Ryan Warren <ryan@war.re>
2015-01-29 19:14:43 -0800

    finished up api client

--- src/root/apiClient.php
+++ src/root/apiClient.php
@@ -96,9 +96,78 @@ class ApiClient{
     return $decoded['admin'] === '1';
   }
 
-  public function getUserInfo($username){
-
+  public function getUserInfo($cookie){
+    //TODO GetUserInfo
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/GetUserInfo");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
     //TODO
+    //name
+    //email
+    //cardID
+    //authyId
+//    return "";
+  }
+
+  public function getAllUsers($cookie){
+    //Check if admin
+    //TODO GetAllUsers
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/GetAllUsers");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+//    return json_encode($result);
+
+    return array('InactiveUsers' => array(""), "ActiveUsers" => array(""), "Admins" => array(""));
+  }
+
+  public function registerUser($params, $cookie){
+    //TODO RegisterUser
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/RegisterUser");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, array('personName'=>$params['personName'], 'newUsername'=>$params['newUsername'],
+      'newPersonPassword'=>$params['newPersonPassword'], 'newPersonEmail'=>$params['newPersonEmail'],
+      'personAuthyID'=>$params['personAuthyID'], 'personCardID'=>$params['personCardID'],
+      'newPersonIsAdmin'=>$params['newPersonIsAdmin']));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
+    //isset($_POST['personName']) && isset($_POST['username'])&&
+    //isset($_POST['password']) && isset($_POST['email']) && isAdmin() && isset($_POST['admin']
+  }
+
+  public function changeUserType($params, $cookie){
+    //TODO ChangeUserType
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/ChangeUserType");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, array('userToChange'=>$params['userToChange'], 'newType'=>$params['newType']));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
+  }
+
+  public function updateUserInfo($params, $cookie){
+    //TODO UpdateUserInfo
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/UpdateUserInfo");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, array('username'=>$params['username'],'old-password'=>$params['old-password'],
+        'new-password'=>$params['new-password'], 'confirm-new-password' => $params['confirm-new-password'],
+        'authy'=>$params['authy'], 'card'=>$params['card'], 'email'=>$params['email'], 'name'=>$params['name']));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
     //name
     //email
     //cardID
@@ -106,49 +175,68 @@ class ApiClient{
     //TAKE currentPassword
     //Take new PASsword
     //Take confirm new PASsword
-    return "";
   }
 
-  public function getAllUsers($username){
-    //Check if admin
+//  public function changePassword(){
+////NOt this??
+//  }
 
-    return array('InactiveUsers' => array(""), "ActiveUsers" => array(""), "Admins" => array(""));
+  public function forgotPassword($params){
+    //TODO ForgotPassword
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/ForgotPassword");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey"));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, array('resetToken'=>$params['resetToken'],'pass'=>$params['pass'],
+                      'confirmPass' => $params['confirmPass']));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
   }
 
-  public function registerUser($username){
-
-  }
-
-  public function changeUser($username){
-
-  }
-
-  public function updateUserInfo($username){
-
-  }
-
-  public function changePassword(){
-
-  }
-
-  public function forgotPassword(){
-
-  }
-
-  public function resetPassword($username){
-
+  public function resetPassword($params, $cookie){
+    //TODO ResetPassword
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/ResetPassword");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, array('username'=>$params['Username'],'email'=>$params['Email']));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
   }
 
   public function lockStatus(){
-
+    //TODO LockStatus
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/LockStatus");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
   }
 
-  public function lock(){
-
+  public function lock($cookie){
+    //TODO lock
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/Lock");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
   }
 
-  public function unlock(){
-
+  public function unlock($cookie){
+    //TODO unlock
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, "$this->apiUrl/Unlock");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-DoorLock-Api-Key: $this->apiKey", "sid: $cookie"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_encode($result);
   }
 
 }
